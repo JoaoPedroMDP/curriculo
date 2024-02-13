47bcf307ef112b84daf014845756932d9556c18b
João Pedro <Joao.Pedro.MDP@outlook.com>
2024-02-13 14:51:00 -0300

    change: Imagem redonda

--- curriculo/app/page.tsx
+++ curriculo/app/page.tsx
@@ -1,10 +1,19 @@
+import Image from "next/image";
+
 export default function Home() {
     let imagePath = "/beach_background.jpg";
     
     return(
         <section>
             <section style={{backgroundImage: `url(${imagePath})`}} className="bg-cover bg-center flex">
-                <p>Olá mundo!</p>
+                <p>João Pedro Martins de Paula</p>
+                <Image 
+                className="rounded-full" 
+                src={"/author_profile.png"} 
+                width={200}
+                height={300} 
+                alt="Autor do currículo de touca, numa praia nublada com neblina"
+                />
             </section>
         </section>
     );
