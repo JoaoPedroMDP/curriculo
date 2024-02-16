2e03025a1b57c9db815376dc5194bb6ce058a051
João Pedro <Joao.Pedro.MDP@outlook.com>
2024-02-16 19:01:40 -0300

    fix(interval): tipagem de parâmetro

--- curriculo/app/objects/interval.tsx
+++ curriculo/app/objects/interval.tsx
@@ -1,8 +1,8 @@
 export default class Interval{
     start: string
-    end: string
+    end: string 
 
-    constructor(start: string, end: string = null){
+    constructor(start: string, end?: string){
         this.start = start
         this.end = end == null ? "Atual" : end
     }
