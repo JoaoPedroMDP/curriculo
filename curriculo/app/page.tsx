b372efdddd393c32c03e75afb799d3b77d77b80a
João Pedro <dev.jpmpjp00@gmail.com>
2024-02-18 00:30:51 +0000

    change: removi margens de itens do banner pra melhorar responsividade

--- curriculo/app/page.tsx
+++ curriculo/app/page.tsx
@@ -43,7 +43,7 @@ export default async function Home() {
                 alt="Autor do currículo de touca, numa praia nublada com neblina"
                 className="absolute opacity-30 z-[-1] object-cover"
                 />
-                <div className="flex flex-col items-center mx-5 xl:items-start text-center lg:text-start order-2 lg:order-1 mt-10 lg:mt-0 lg:basis-2/3">
+                <div className="flex flex-col items-center xl:items-start text-center lg:text-start order-2 lg:order-1 mt-10 lg:mt-0 lg:basis-2/3">
                     <p className="font-bebas text-[50px] lg:text-[60px] xl:text-[70px] 2xl:text-[96px]">{data.name}</p>
                     <p className="font-raleway text-[25px] lg:text-[22px] xl:text-[28px] 2xl:text-[36px]">{data.title}</p>
                     <p className="font-raleway text-[18px] lg:text-[15px] xl:text-[18px] 2xl:text-[24px]">{data.marital_status} | {age} anos</p>
@@ -51,7 +51,7 @@ export default async function Home() {
                     <p className="font-raleway text-[18px] lg:text-[15px] xl:text-[18px] 2xl:text-[24px]"><a href={data.linkedin} target="blank">Linkedin</a> | <a href={data.github} target="blank">Github</a></p>
                 </div>
 
-                <div className="relative w-[250px] h-[250px] 2xl:w-[400px] 2xl:h-[400px] 2xl:ml-20 order-1 2xl:order-2">
+                <div className="relative w-[250px] h-[250px] 2xl:w-[400px] 2xl:h-[400px] order-1 2xl:order-2">
                     <Image 
                     className="rounded-full" 
                     src={"/author_profile.png"} 
