b5e03c7111c4eb791d5dcb25c34821c90272c3bf
João Pedro <Joao.Pedro.MDP@outlook.com>
2024-02-20 19:26:20 -0300

    change: Atualizei alguns dados

--- curriculo/app/data/data.tsx
+++ curriculo/app/data/data.tsx
@@ -35,7 +35,7 @@ const curriculumData: any = {
                     "start": "01/09/2022",
                     "end": "01/09/2023"
                 },
-                "tools": ["Python", "Selenium"],
+                "tools": ["Python", "Selenium", "Docker"],
                 "skills": ["Escrita acadêmica"],
                 "description": "Pesquisa sobre Web Scraping para obtenção de dados turísticos do Tripadvisor. Desenvolvi um Scrapper, juntamente de um artigo científico sobre o tema."
             },
@@ -57,7 +57,7 @@ const curriculumData: any = {
             "institution": "Rede Nacional de Ensino e Pesquisa - POP Paraná",
             "position": "Técnico de Desenvolvimento",
             "tools": ["Python", "Django", "Docker", "Linux", "Flask", "Ansible", "Git", "Postman", "API REST", "Pytest", "Redis", "Netbox", "Topdesk"],
-            "skills": ["Redes de computadores", "Bots", "APIs do Telegram/Whatsapp"],
+            "skills": [],
             "description": "Após ser promovido, as atividades continuaram praticamente as mesmas. As diferenças estão na carga horária e na participação do rodízio de plantão semanal.",
             "dates": {
                 "start": "01/06/2022",
@@ -70,7 +70,7 @@ const curriculumData: any = {
             "institution": "Rede Nacional de Ensino e Pesquisa - POP Paraná",
             "position": "Estagiário de desenvolvimento",
             "tools": ["Python", "Django", "Docker", "Linux", "Flask", "Ansible", "Git", "Postman", "API REST", "Bots", "Pytest", "Redis", "Netbox", "Topdesk"],
-            "skills": ["Redes de computadores", "APIs do Telegram/Whatsapp"],
+            "skills": ["Coleta de Requisitos", "Desenvolvimento de sistemas", "Planejamento de sistemas", "Gestão de projetos"],
             "description": "Desenvolvimento de vários scripts de automação com Python, bem como bots para Telegram/Whatsapp que agilizam a resolução de incidentes e a comunicação com as universidades parceiras. Integrei diversos serviços nos bots, como o Topdesk (Gerenciamento de Tickets), Netbox (Gerenciamento de recursos de Rede) e Icinga (Monitoramento de rede).",
             "dates": {
                 "start": "04/10/2021",
@@ -82,8 +82,8 @@ const curriculumData: any = {
             "type": "job",
             "institution": "Clientar CRM",
             "position": "Estagiário desenvolvedor Back-End",
-            "tools": ["Laravel", "Mysql", "Dbeaver", "Postman", "API REST"],
-            "skills": ["Desenvolvimento de Back-end", "Planejamento"],
+            "tools": ["Laravel", "Mysql", "Dbeaver", "Postman", "API REST", "Back-end"],
+            "skills": ["Planejamento de sistemas"],
             "description": "Desenvolvimento de back-end para um sistema de CRM. Utilizei Laravel e MySQL. Supervisionado nos 4 primeiros meses, aprendi Clean Code e maneiras mais robustas de se organizar o código. Depois, meu supervisor saiu e fiquei sozinho no desenvolvimento da API. Planejei e comecei o desenvolvimento de um módulo de versionamento de orçamentos e impostos.",
             "dates": {
                 "start": "11/01/2021",
@@ -96,7 +96,7 @@ const curriculumData: any = {
             "institution": "Ecomp - Empresa Júnior de Computação da UFPR",
             "position": "Gerente de Qualidade",
             "tools": ["Linux", "Git", "Deploy", "SSH"],
-            "skills": ["Gestão de pessoas",  "Administração de sistemas", "Capacitação de novos membros"],
+            "skills": ["Gestão de pessoas",  "Administração de sistemas", "Condução de reuniões", "Capacitação de novos membros"],
             "description": "Responsável por realizar deploy de aplicações em servidores na nuvem. Gestão de pessoas, planejamento de OKR's. Fui para Qualidade pois a diretoria passou por uma crise e ficou praticamente sem membros, ficando apenas eu e outra pessoa. Preparamos um plano de reestruturação da diretoria e conseguimos terminar 2021 com o prêmio de Diretoria do Ano.",
             "dates": {
                 "start": "01/09/2021",
@@ -108,8 +108,8 @@ const curriculumData: any = {
             "type": "job",
             "institution": "Ecomp - Empresa Júnior de Computação da UFPR",
             "position": "Assessor de negócios",
-            "tools": ["Scrum", "Backlog", "Funil de vendas", "Persona", "Planning Poker", "Briefing"],
-            "skills": ["Planejamento de sistemas", "Comunicação com cliente", "Condução de reuniões"],
+            "tools": ["Scrum", "Backlog", "Funil de vendas", "Persona"],
+            "skills": ["Planejamento de sistemas", "Comunicação com cliente", "Condução de reuniões", "Planning Poker", "Briefing"],
             "description": "Responsável por entrar em contato com potenciais clientes ('leads') (prospecção passiva por meio de Google Ads), realizar reuniões de briefing, montar documento de funcionalidades para revisão do cliente, conduzir reuniões de Planning Poker com os desenvolvedores. Também fui P.O de um projeto em andamento com mestrandas de Enfermagem, cujos produtos finais foram 5 aplicativos que atualmente se encontram na Play/App store do Governo Federal.",
             "dates": {
                 "start": "01/01/2021",
@@ -122,7 +122,7 @@ const curriculumData: any = {
             "institution": "Ecomp - Empresa Júnior de Computação da UFPR",
             "position": "Assessor de Projetos",
             "tools": ["Scrum", "Gitlab", "OBS Studio"],
-            "skills": ["Resolução de problemas", "Falar em público", "Capacitação de novos membros"],
+            "skills": ["Resolução de problemas", "Falar em público", "Capacitação de novos membros", "Inovação"],
             "description": "Responsável por auxiliar na gestão de projetos, procurando por melhorias e otimizações no processo de desenvolvimento de software. Também responsável pela capacitação de novos membros (Realizei 03 capacitações ao longo de toda a minha estadia na empresa)",
             "dates": {
                 "start": "01/04/2020",
@@ -134,8 +134,8 @@ const curriculumData: any = {
             "type": "job",
             "institution": "Ecomp - Empresa Júnior de Computação da UFPR",
             "position": "Desenvolvedor Back-End",
-            "tools": ["Laravel", "PHP", "MariaDB", "Git", "Scrum", "Postman", "PagSeguro", "Linux", "API REST"],
-            "skills": ["Trabalhar sob pressão", "Trabalho em equipe", "Resolução de problemas"],
+            "tools": ["Laravel", "PHP", "MariaDB", "Git", "Scrum", "Postman", "PagSeguro", "Linux", "API REST", "Back-end"],
+            "skills": ["Trabalhar sob pressão", "Trabalho em equipe", "Planejamento de sistemas"],
             "description": "Responsável por desenvolver back-ends. Participei de quase todos os projetos dentro da empresa, tanto aqueles aos quais fui alocado, quanto outros projetos que precisavam de ajuda.",
             "dates": {
                 "start": "01/04/2020",
