2620662ab72c2715cb93dcc3cefcd6cac444a448
João Pedro <Joao.Pedro.MDP@outlook.com>
2024-02-28 10:00:48 -0300

    change: TIrei o chunking dos learnables. Não ajudava muito

--- curriculo/app/components/sections/learnables.tsx
+++ curriculo/app/components/sections/learnables.tsx
@@ -1,39 +1,25 @@
 import { Learnable, Skill, Tool } from "../../objects/learnables";
 import LearnableC from "../learnableC";
 
-function chunkArray(array: any[], size: number){
-    let chunkedArray = [];
-    for(let i = 0; i < array.length; i += size){
-        chunkedArray.push(array.slice(i, i + size));
-    }
-    return chunkedArray;
-}
 
 export default function Learnables({skills, tools}: 
     {skills: Skill[], tools: Tool[], learnableFilters: string[], updateLearnablesFilter: any})
 {
-    let chunkedSkills = chunkArray(skills, 4);
-    let chunkedTools = chunkArray(tools, 4);
 
     return(
-        <section id="learnables" className="flex flex-row justify-center align-middle w-full">
-            <div className="flex flex-col flex-grow">
-                {chunkedTools.map((chunk, index) => 
-                    <div id="tools" key={index} className="flex flex-row flex-wrap justify-evenly bg-lightBlue align-middle">
-                    {chunk.map((tool) => 
+        <section id="learnables" className="flex flex-row flex-wrap align-middle w-full">
+            <div className="flex flex-col lg:flex-grow">
+                <div id="tools" className="flex flex-row flex-wrap bg-lightBlue align-middle">
+                    {tools.map((tool) => 
                         <LearnableC key={tool.id} learnable={tool.toObject()}/>
                     )}
-                    </div>    
-                )}
-            </div>
-            <div className="flex flex-col flex-grow">
-                {chunkedSkills.map((chunk, index) => 
-                    <div id="skills" key={index} className="flex flex-row flex-wrap justify-evenly bg-yellow align-middle">
-                    {chunk.map((skill) => 
+                </div>            </div>
+            <div className="flex flex-col lg:flex-grow">
+                <div id="skills" className="flex flex-row flex-wrap bg-yellow align-middle">
+                    {skills.map((skill) => 
                         <LearnableC key={skill.id} learnable={skill.toObject()}/>
                     )}
-                    </div>    
-                )}
+                </div>
             </div>
         </section>
     );
