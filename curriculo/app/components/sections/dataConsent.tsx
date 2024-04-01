14e990ebec8b3d6e15dd05ba623f62edc4945c8e
João Pedro <dev.jpmpjp00@gmail.com>
2024-04-01 14:46:53 +0000

    fix: Faltou escapar as aspas simples

--- curriculo/app/components/sections/dataConsent.tsx
+++ curriculo/app/components/sections/dataConsent.tsx
@@ -2,7 +2,7 @@ export default function DataConsentSection({consented, updateConsent} : {consent
     if(consented === undefined){
         return(
             <section id="data-consent" className="print:hidden animate-slideIn fixed bottom-0 bg-white text-black w-full px-5 py-4 flex flex-col text-center font-raleway border-yellow border-t-8">
-                <p className="text-[20px] font-bold">A 'Oráculo' lhe oferece cookies. Aceitas?</p>
+                <p className="text-[20px] font-bold">A &lsquo;Oráculo&rsquo; lhe oferece cookies. Aceitas?</p>
                 <p className="text-[18px]">Estão saborosos, hehehe</p>
                 <div className="flex flex-row justify-around lg:justify-center lg:gap-20 mt-2 pt-5">
                     <button onClick={() => updateConsent(false)} className="font-semibold border-[2px] border-black bg-zinc-200 px-5 py-1">Rejeitar</button>
